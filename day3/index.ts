0edf24ca51da1e5b6cbc9dcc4deb9b4d3d990a0e
Jacob Odden <jacob.l.odden@gmail.com>
2020-12-04 15:43:38 -0500

    feat(day3): finish part 2 for day 3

--- day3/index.ts
+++ day3/index.ts
@@ -341,6 +341,63 @@ function part1 () {
 }
 
 function part2 () {
+  const travlers = [
+    {
+      x: 0,
+      y: 0,
+      dx: 1,
+      dy: 1,
+      tc: 0
+    },
+    {
+      x: 0,
+      y: 0,
+      dy: 3,
+      dx: 1,
+      tc: 0
+    },
+    {
+      x: 0,
+      y: 0,
+      dy: 5,
+      dx: 1,
+      tc: 0
+    },
+    {
+      x: 0,
+      y: 0,
+      dy: 7,
+      dx: 1,
+      tc: 0
+    },
+    {
+      x: 0,
+      y: 0,
+      dy: 1,
+      dx: 2,
+      tc: 0
+    }
+  ]
+
+  const visitMap = new Map()
+
+  for (const t of travlers) {
+    do {
+      t.x += t.dx
+      t.y = (t.y + t.dy) % 31
+      if (visitMap.has(`${t.x}_${t.y}`)) {
+        if (visitMap.get(`${t.x}_${t.y}`)) {
+          t.tc++
+        }
+      } else {
+        if (t.x > 322) break
+        if (map[t.x][t.y]) { t.tc++ }
+        visitMap.set(`${t.x}_${t.y}`, map[t.x][t.y])
+      }
+    } while (t.x < 322)
+  }
+
+  return travlers.reduce((a, c) => c.tc * a, 1)
 }
 
 console.log('Part 1:', part1())
